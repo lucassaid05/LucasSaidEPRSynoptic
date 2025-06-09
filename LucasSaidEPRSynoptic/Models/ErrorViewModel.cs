f5e942ed7133a4c670860f391ddcda85434a4fbc
Lucas Said <lucas.said.g52506@mcast.edu.mt>
2025-06-09 15:04:25 +0200

    Add project files.

---
+++ LucasSaidEPRSynoptic/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LucasSaidEPRSynoptic.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
